02d910cf8c971a9e0652af2017ac9fca0f6494f6
vamshivuppula <vv1020@wildcats.unh.edu>
2020-06-09 20:47:50 -0400

    made changes to Course and TransferEval models

--- transfer/views/remove_data.py
+++ transfer/views/remove_data.py
@@ -1,6 +1,6 @@
 from ..models.model_major import Major
 from ..models.model_school import School
-from ..models.model_course import Course
+from ..models.model_transfer_course import TransferCourse
 from ..models.model_major_requirement import Major_requirement
 from ..models.model_transferevaluation import Transferevaluation
 from ..models.model_approver import Approver
@@ -11,7 +11,7 @@ from django.shortcuts import render, get_object_or_404
 def remove_all_data(request):
     Major.objects.all().delete()
     School.objects.all().delete()
-    Course.objects.all().delete()
+    TransferCourse.objects.all().delete()
     Major_requirement.objects.all().delete()
     Transferevaluation.objects.all().delete()
     Approver.objects.all().delete()
