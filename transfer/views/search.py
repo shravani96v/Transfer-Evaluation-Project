d7bf6da63325e66392f7db4f11e469d4e9ca8972
shravani96v <sv1059@wildcats.unh.edu>
2020-08-06 13:08:35 -0400

    Changing template to fix merge conflcits

--- transfer/views/search.py
+++ transfer/views/search.py
@@ -25,8 +25,10 @@ def search(request):
     major = Major.objects.all()
     transfereval = Transferevaluation.objects.all()
     # transfereval.paginate(page=request.GET.get("page", 1), per_page=20)
-    paginator = Paginator(transfereval, 5)
+    paginator = Paginator(transfereval, 10)
     page_number = request.GET.get('page')
+
+
     page_obj = paginator.get_page(page_number)
 
     if request.method == "POST":
@@ -35,25 +37,43 @@ def search(request):
 
         if majorid == "null" and schoolid == "null":
             transfereval = Transferevaluation.objects.all()
-            return render(request, 'home_paginated.html', {"major": major, "schools": schools, 'object_list': transfereval})
+            paginator = Paginator(transfereval, 10)
+            page_number = request.GET.get('page')
+            page_obj = paginator.get_page(page_number)
+            print(page_obj)
+            return render(request, 'home_paginated.html', {"major": major, "schools": schools, 'page_obj': page_obj})
         elif majorid == "null" and schoolid != "null":
            school = School.objects.filter(school_id=schoolid)
            course = TransferCourse.objects.filter(school_id__in = school)
            transfereval = Transferevaluation.objects.filter(transfer_course_id__in = course)
-           return render(request, 'home_paginated.html', {"major": major, "schools": schools, 'object_list': transfereval})
+           paginator = Paginator(transfereval, 10)
+           page_number = request.GET.get('page')
+           page_obj = paginator.get_page(page_number)
+
+           return render(request, 'home_paginated.html', {"major": major, "schools": schools, 'page_obj': page_obj})
         elif majorid != "null" and schoolid == "null":
            major_filter = Major.objects.filter(major_id=majorid)
            major_req = Major_requirement.objects.filter(major_id__in=major_filter)
            transfereval = Transferevaluation.objects.filter(major_req_id__in = major_req)
-           return render(request, 'home_paginated.html', {"major": major, "schools": schools, 'object_list': transfereval})
+           paginator = Paginator(transfereval, 10)
+           page_number = request.GET.get('page')
+           page_obj = paginator.get_page(page_number)
+
+
+
+           return render(request, 'home_paginated.html', {"major": major, "schools": schools, 'page_obj': page_obj})
         else:
            school = School.objects.filter(school_id=schoolid)
            course = TransferCourse.objects.filter(school_id__in = school)
            major_filter = Major.objects.filter(major_id=majorid)
            major_req = Major_requirement.objects.filter(major_id__in=major_filter)
+
            major_transfereval = Transferevaluation.objects.filter(major_req_id__in = major_req).filter(transfer_course_id__in = course)
-           return render(request, 'home_paginated.html', {"major": major, "schools": schools, 'object_list': major_transfereval})
-    return render(request, 'home_paginated.html', {"major": major, "schools": schools, "object_list": transfereval, "pagination": page_obj})
+           paginator = Paginator(major_transfereval, 10)
+           page_number = request.GET.get('page')
+           page_obj = paginator.get_page(page_number)
+           return render(request, 'home_paginated.html', {"major": major, "schools": schools, 'page_obj': page_obj})
+    return render(request, 'home_paginated.html', {"major": major, "schools": schools, "page_obj": page_obj, 'page_number':page_number})
 
 
 def search_ajax(request):
